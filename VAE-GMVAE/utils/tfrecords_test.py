37f1668b1d33b62c3663e9813be2d7aa50100cd6
aihanb <nxianrui@163.com>
2019-08-08 17:08:09 +0200

    Add files via upload

--- VAE-GMVAE/utils/tfrecords_test.py
+++ VAE-GMVAE/utils/tfrecords_test.py
@@ -32,50 +32,98 @@ import numpy as np
 # for example in tf.python_io.tf_record_iterator("data_1.tfrecords"):
 #     print(tf.train.Example.FromString(example))
 
-def _parse_function(self, example_proto):
-    """
+# def _parse_function(self, example_proto):
+#     """
+#
+#     Creates parse function for loading TFRecords files
+#
+#
+#
+#     :param example_proto:   prototype coming from a TFRecords file
+#
+#     :return:                data from TFRecords files
+#
+#     """
+#
+#     # with tf.variable_scope('DataFeedingHelper/parse_function'):
+#     keys_to_features = {'x': tf.FixedLenFeature(self._dim_features, tf.float32),
+#
+#                         'y': tf.FixedLenFeature(self._dim_hard, tf.float32)}
+#     print('keys_to_features: ', keys_to_features)
+#
+#     parsed_features = tf.parse_single_example(example_proto, keys_to_features)
+#
+#     print("parsed_features['x']: ", parsed_features['x'])
+#     print("parsed_features['y']: ", parsed_features['y'])
+#
+#     return parsed_features['x'], parsed_features['y']
+#
+# # filenames = ["data_1.tfrecords"]
+# # dataset = tf.data.TFRecordDataset(filenames)
+# # dataset = dataset.map(_parse_function)
+#
+# def _input_fn(self):
+#     """
+#
+#     Create Dataset using TF-API and iterate through the dict
+#
+#     """
+#
+#     with tf.variable_scope('DataFeedingHelper/input_fn'):
+#         # get self.train, self.test and selt.dev references
+#
+#         for key, item in self._dict_lists.items():
+#             dataset = tf.data.TFRecordDataset(item)
+#
+#             # Parse the record into tensors.
+#
+#             dataset = dataset.map(self._parse_function)
+#
+#             dataset = dataset.shuffle(100000, reshuffle_each_iteration=False)
+#
+#             dataset = dataset.batch(self._batch_size, drop_remainder=True)
+#
+#             # dict_ref[key] = dataset.make_initializable_iterator()
+#
+#             setattr(self, key, dataset.make_initializable_iterator())
 
-    Creates parse function for loading TFRecords files
+
+def dataset_input_fn():
+    filenames = ["data_1.tfrecords"]
+    dataset = tf.data.TFRecordDataset(filenames)
+
+    def _parse_function(record):
+        """
+
+        Creates parse function for loading TFRecords files
 
 
 
-    :param example_proto:   prototype coming from a TFRecords file
+        :param example_proto:   prototype coming from a TFRecords file
 
-    :return:                data from TFRecords files
+        :return:                data from TFRecords files
 
-    """
+        """
 
-    with tf.variable_scope('DataFeedingHelper/parse_function'):
+        # with tf.variable_scope('DataFeedingHelper/parse_function'):
         keys_to_features = {'x': tf.FixedLenFeature(self._dim_features, tf.float32),
 
                             'y': tf.FixedLenFeature(self._dim_hard, tf.float32)}
+        print('keys_to_features: ', keys_to_features)
 
-        parsed_features = tf.parse_single_example(example_proto, keys_to_features)
+        parsed_features = tf.parse_single_example(record, keys_to_features)
+
+        print("parsed_features['x']: ", parsed_features['x'])
+        print("parsed_features['y']: ", parsed_features['y'])
 
         return parsed_features['x'], parsed_features['y']
 
+    dataset = dataset.map(_parse_function)
+    dataset = dataset.shuffle(100000, reshuffle_each_iteration=False)
+    dataset = dataset.batch(32)
+    dataset = dataset.repeat(10)
 
-def _input_fn(self):
-    """
+    return dataset
 
-    Create Dataset using TF-API and iterate through the dict
-
-    """
-
-    with tf.variable_scope('DataFeedingHelper/input_fn'):
-        # get self.train, self.test and selt.dev references
-
-        for key, item in self._dict_lists.items():
-            dataset = tf.data.TFRecordDataset(item)
-
-            # Parse the record into tensors.
-
-            dataset = dataset.map(self._parse_function)
-
-            dataset = dataset.shuffle(100000, reshuffle_each_iteration=False)
-
-            dataset = dataset.batch(self._batch_size, drop_remainder=True)
-
-            # dict_ref[key] = dataset.make_initializable_iterator()
-
-            setattr(self, key, dataset.make_initializable_iterator())
+dataset = dataset_input_fn()
+print('dataset: ', dataset)
