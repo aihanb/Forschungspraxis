dbddcb2cfd791d69fffc69394d5fee6f00464754
aihanb <nxianrui@163.com>
2019-09-06 13:14:37 +0200

    Add files via upload

--- VAE-GMVAE/utils/tfrecords_test.py
+++ VAE-GMVAE/utils/tfrecords_test.py
@@ -11,119 +11,98 @@ import tensorflow as tf
 import numpy as np
 
 
-# def read_test(input_file):
-#
-#     # use dataset to load the tfrecords
-#     dataset = tf.data.TFRecordDataset(input_file)
-#     print('dataset: ', dataset)
-#     # dataset = dataset.map(_parse_record)
-#     # iterator = dataset.make_one_shot_iterator()
-#     #
-#     # with tf.Session() as sess:
-#     #     features = sess.run(iterator.)
-#
-#
-#
-# read_test('test.tfrecords')
-# read_test('data_1.tfrecords')
-#
-# # record_iterator = tf.python_io.tf_record_iterator()
+def _parse_function(example_proto):
+    """
 
-# for example in tf.python_io.tf_record_iterator("data_1.tfrecords"):
-#     print(tf.train.Example.FromString(example))
-
-# def _parse_function(self, example_proto):
-#     """
-#
-#     Creates parse function for loading TFRecords files
-#
-#
-#
-#     :param example_proto:   prototype coming from a TFRecords file
-#
-#     :return:                data from TFRecords files
-#
-#     """
-#
-#     # with tf.variable_scope('DataFeedingHelper/parse_function'):
-#     keys_to_features = {'x': tf.FixedLenFeature(self._dim_features, tf.float32),
-#
-#                         'y': tf.FixedLenFeature(self._dim_hard, tf.float32)}
-#     print('keys_to_features: ', keys_to_features)
-#
-#     parsed_features = tf.parse_single_example(example_proto, keys_to_features)
-#
-#     print("parsed_features['x']: ", parsed_features['x'])
-#     print("parsed_features['y']: ", parsed_features['y'])
-#
-#     return parsed_features['x'], parsed_features['y']
-#
-# # filenames = ["data_1.tfrecords"]
-# # dataset = tf.data.TFRecordDataset(filenames)
-# # dataset = dataset.map(_parse_function)
-#
-# def _input_fn(self):
-#     """
-#
-#     Create Dataset using TF-API and iterate through the dict
-#
-#     """
-#
-#     with tf.variable_scope('DataFeedingHelper/input_fn'):
-#         # get self.train, self.test and selt.dev references
-#
-#         for key, item in self._dict_lists.items():
-#             dataset = tf.data.TFRecordDataset(item)
-#
-#             # Parse the record into tensors.
-#
-#             dataset = dataset.map(self._parse_function)
-#
-#             dataset = dataset.shuffle(100000, reshuffle_each_iteration=False)
-#
-#             dataset = dataset.batch(self._batch_size, drop_remainder=True)
-#
-#             # dict_ref[key] = dataset.make_initializable_iterator()
-#
-#             setattr(self, key, dataset.make_initializable_iterator())
-
-
-def dataset_input_fn():
-    filenames = ["data_1.tfrecords"]
-    dataset = tf.data.TFRecordDataset(filenames)
-
-    def _parse_function(record):
-        """
-
-        Creates parse function for loading TFRecords files
+    Creates parse function for loading TFRecords files
 
 
 
-        :param example_proto:   prototype coming from a TFRecords file
+    :param example_proto:   prototype coming from a TFRecords file
 
-        :return:                data from TFRecords files
+    :return:                data from TFRecords files
 
-        """
+    """
 
-        # with tf.variable_scope('DataFeedingHelper/parse_function'):
-        keys_to_features = {'x': tf.FixedLenFeature(self._dim_features, tf.float32),
+    # with tf.variable_scope('DataFeedingHelper/parse_function'):
+    keys_to_features = {'x': tf.FixedLenFeature([39], tf.float32),
 
-                            'y': tf.FixedLenFeature(self._dim_hard, tf.float32)}
-        print('keys_to_features: ', keys_to_features)
+                        'y': tf.FixedLenFeature([1], tf.float32)}
 
-        parsed_features = tf.parse_single_example(record, keys_to_features)
+    parsed_features = tf.parse_single_example(example_proto, keys_to_features)
 
-        print("parsed_features['x']: ", parsed_features['x'])
-        print("parsed_features['y']: ", parsed_features['y'])
+    # print("parsed_features['x']: ", parsed_features['x'])
+    # print("parsed_features['y']: ", parsed_features['y'])
 
-        return parsed_features['x'], parsed_features['y']
+    return parsed_features['x'], parsed_features['y']
 
+
+def get_dataset(fname):
+    """
+
+    Create Dataset using TF-API
+
+
+    :param fname:   filenames
+
+    :return:        dataset from TFRecords files
+
+    """
+    dataset = tf.data.TFRecordDataset(fname)
     dataset = dataset.map(_parse_function)
-    dataset = dataset.shuffle(100000, reshuffle_each_iteration=False)
-    dataset = dataset.batch(32)
-    dataset = dataset.repeat(10)
-
+    # dataset = dataset.shuffle(buffer_size=100000)
+    dataset = dataset.batch(138420)
+    # dataset = dataset.batch(10000)
+    # dataset = dataset.batch(1)
+    # dataset = dataset.repeat(1)
+    # dataset = dataset.shuffle(buffer_size=1000).batch(32).repeat(10)
     return dataset
 
-dataset = dataset_input_fn()
-print('dataset: ', dataset)
+
+dataset = get_dataset('data_1.tfrecords')
+iterator = dataset.make_initializable_iterator()
+x, y = iterator.get_next()
+
+with tf.Session() as sess:
+    sess.run(iterator.initializer)
+    try:
+        while True:
+            data, label = sess.run([x, y])
+            # print("type(data): ", type(data))
+            # print("data.shape: ", data.shape)
+            # print("type(label): ", type(label))
+            # print("label.shape: ", label.shape)
+    except tf.errors.OutOfRangeError:
+        print("END!")
+
+print("type(data): ", type(data))
+print("data.shape: ", data.shape)
+print("type(label): ", type(label))
+print("label.shape: ", label.shape)
+# print("data: ", data)
+# print("label: ", label)
+data_dim = data.shape[1]
+num_data = data.shape[0]
+train_size = int(num_data*0.8)
+valid_size = int(num_data*0.1)
+test_size = num_data - train_size - valid_size
+print("train_size: ", train_size)
+print("valid_size: ", valid_size)
+print("test_size: ", test_size)
+
+x_train = data[:train_size]
+x_valid = data[train_size:(train_size+valid_size)]
+x_test = data[(train_size+valid_size):]
+
+x_train = np.reshape(x_train, [-1, 39, 1, 1])
+x_valid = np.reshape(x_valid, [-1, 39, 1, 1])
+x_test = np.reshape(x_test, [-1, 39, 1, 1])
+
+print("////// After np.reshape //////")
+print("x_train.shape: ", x_train.shape)
+print("x_valid.shape: ", x_valid.shape)
+print("x_test.shape: ", x_test.shape)
+
+# train_dataset = Dataset(x_train, data.train.labels)
+# valid_dataset = Dataset(x_valid, data.train.labels)
+# test_dataset = Dataset(x_test, data.test.labels)
--- VAE-GMVAE/utils/utils.py
+++ VAE-GMVAE/utils/utils.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 from __future__ import division
 import math
 import random
@@ -24,7 +27,6 @@ import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
 from utils.dataset import Dataset
-
 from scipy.io import loadmat
 '''  ------------------------------------------------------------------------------
                                     DATA METHODS
@@ -34,8 +36,6 @@ def get_data(data_file):
     data_path = '../data/'
     if(data_file == 'MNIST'):
         data_path +='MNIST_data'
-    if(data_file == 'FREY'):
-        data_path +='frey_rawface.mat'
     if(data_file == 'CIFAR10'):
         data_path +='MNIST_data'
     return 
@@ -47,8 +47,120 @@ def load_data(dataset_name):
         return load_MNIST()
     elif(dataset_name == 'FREY'):
         return load_FREY()
+    elif(dataset_name == 'TF'):
+        return load_TF()
 
     return None
+
+def load_TF():
+    def _parse_function(example_proto):
+        """
+
+        Creates parse function for loading TFRecords files
+
+
+
+        :param example_proto:   prototype coming from a TFRecords file
+
+        :return:                data from TFRecords files
+
+        """
+
+        # with tf.variable_scope('DataFeedingHelper/parse_function'):
+        keys_to_features = {'x': tf.FixedLenFeature([39], tf.float32),
+
+                            'y': tf.FixedLenFeature([1], tf.float32)}
+
+        parsed_features = tf.parse_single_example(example_proto, keys_to_features)
+
+        # print("parsed_features['x']: ", parsed_features['x'])
+        # print("parsed_features['y']: ", parsed_features['y'])
+
+        return parsed_features['x'], parsed_features['y']
+
+    def get_dataset(fname):
+        """
+
+        Create Dataset using TF-API
+
+
+        :param fname:   filenames
+
+        :return:        dataset from TFRecords files
+
+        """
+        dataset = tf.data.TFRecordDataset(fname)
+        dataset = dataset.map(_parse_function)
+        # dataset = dataset.shuffle(buffer_size=100000)
+        dataset = dataset.batch(138420)
+        # dataset = dataset.batch(10000)
+        # dataset = dataset.batch(1)
+        # dataset = dataset.repeat(1)
+        # dataset = dataset.shuffle(buffer_size=1000).batch(32).repeat(10)
+        return dataset
+
+    dataset = get_dataset('../data/data_1.tfrecords')
+    iterator = dataset.make_initializable_iterator()
+    x, y = iterator.get_next()
+
+    with tf.Session() as sess:
+        sess.run(iterator.initializer)
+        try:
+            while True:
+                data, label = sess.run([x, y])
+                # print("type(data): ", type(data))
+                # print("data.shape: ", data.shape)
+                # print("type(label): ", type(label))
+                # print("label.shape: ", label.shape)
+        except tf.errors.OutOfRangeError:
+            print("END!")
+
+    print("type(data): ", type(data))
+    print("data.shape: ", data.shape)
+    print("type(label): ", type(label))
+    print("label.shape: ", label.shape)
+    # print("data: ", data)
+    # print("label: ", label)
+    data_dim = data.shape[1]
+    num_data = data.shape[0]
+    train_size = int(num_data * 0.8)
+    valid_size = int(num_data * 0.1)
+    test_size = num_data - train_size - valid_size
+    print("train_size: ", train_size)
+    print("valid_size: ", valid_size)
+    print("test_size: ", test_size)
+
+    x_train = data[:train_size]
+    x_valid = data[train_size:(train_size + valid_size)]
+    x_test = data[(train_size + valid_size):]
+
+    x_train = np.reshape(x_train, [-1, 39, 1, 1])
+    x_valid = np.reshape(x_valid, [-1, 39, 1, 1])
+    x_test = np.reshape(x_test, [-1, 39, 1, 1])
+
+    print("////// After np.reshape //////")
+    print("x_train.shape: ", x_train.shape)
+    print("x_valid.shape: ", x_valid.shape)
+    print("x_test.shape: ", x_test.shape)
+
+    train_dataset = Dataset(x_train, label)
+    valid_dataset = Dataset(x_valid, label)
+    test_dataset = Dataset(x_test, label)
+
+    print("```````````")
+    print("train_dataset.height: ", train_dataset.height)
+    print("train_dataset.width: ", train_dataset.width)
+    print("train_dataset.num_channels: ", train_dataset.num_channels)
+    print("```````````")
+
+
+
+    print('Train Data: ', train_dataset.x.shape)
+    print('Valid Data: ', valid_dataset.x.shape)
+    print('Test Data: ', test_dataset.x.shape)
+    print("label: ", label)
+    print("label.shape: ", label.shape)
+    return train_dataset, valid_dataset, test_dataset
 
 def load_FREY():
     data_path = '../data/frey_rawface.mat'
@@ -93,12 +205,18 @@ def load_FREY():
 
 
 def load_MNIST():
+
+
     data_path = '../data/MNIST_data'
     data = input_data.read_data_sets(data_path, one_hot=False)
     x_train_aux = data.train.images
     x_test = data.test.images
     data_dim = data.train.images.shape[1]
     n_train = data.train.images.shape[0]
+    print("TTTTTTTTTTTTTTTTTTTTTTTTTT")
+    print("data.train.images.shape: ", data.train.images.shape)
+    print("data_dim: ", data_dim)
+    print("n_train: ", n_train)
 
     train_size = int(n_train * 0.8)
     valid_size = n_train - train_size
@@ -107,19 +225,36 @@ def load_MNIST():
           ':', time.localtime().tm_min, 'h')
     # logs.write('\tData loaded ' + str(time.localtime().tm_hour) +':' + str(time.localtime().tm_min) + 'h\n')
 
+    print('x_train.shape: ', x_train.shape)
     x_train = np.reshape(x_train, [-1, 28, 28, 1])
     x_valid = np.reshape(x_valid, [-1, 28, 28, 1])
     x_test = np.reshape(x_test, [-1, 28, 28, 1])
+
+    print("!!!!!!!!!!!!!!!!!!!")
+    print("Dataset test after reshape: ")
+    print('x_train.shape: ', x_train.shape)
+    print("type(x_train): ", type(x_train))
+    print('type(data.train.labels): ', type(data.train.labels))
+    print("!!!!!!!!!!!!!!!!!!!")
 
 
     train_dataset = Dataset(x_train, data.train.labels)
     valid_dataset = Dataset(x_valid, data.train.labels)
     test_dataset = Dataset(x_test, data.test.labels)
 
+    print("```````````")
+    print("train_dataset.height: ", train_dataset.height)
+    print("train_dataset.width: ", train_dataset.width)
+    print("train_dataset.num_channels: ", train_dataset.num_channels)
+    print("```````````")
+
+
+
     print('Train Data: ', train_dataset.x.shape)
     print('Valid Data: ', valid_dataset.x.shape)
     print('Test Data: ', test_dataset.x.shape)
-
+    print("train_dataset.labels: ", train_dataset.labels)
+    print("data.train.labels.shape: ", data.train.labels.shape)
     return train_dataset, valid_dataset, test_dataset
 
 
